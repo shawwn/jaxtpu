a5c719245e73a5a592760dfa030b125d503d7dbc
Shawn Presser <shawnpresser@gmail.com>
2021-08-10 23:45:18 -0500

    Version 0.1.15

--- setup.py
+++ setup.py
@@ -29,10 +29,6 @@ class PreDevelopCommand(develop):
         super().__init__(*args, **kwargs)
         jaxtpu_preinstall()
         atexit.register(jaxtpu_postinstall)
-    # def run(self):
-    #     jaxtpu_preinstall()
-    #     develop.run(self)
-    #     jaxtpu_postinstall()
 
 class PreInstallCommand(install):
     """Pre-installation for installation mode."""
@@ -40,10 +36,6 @@ class PreInstallCommand(install):
         super().__init__(*args, **kwargs)
         jaxtpu_preinstall()
         atexit.register(jaxtpu_postinstall)
-    # def run(self):
-    #     jaxtpu_preinstall()
-    #     install.run(self)
-    #     jaxtpu_postinstall()
 
 
 packages = \
@@ -54,7 +46,7 @@ package_data = \
 
 setup_kwargs = {
     'name': 'jaxtpu',
-    'version': '0.1.14',
+    'version': '0.1.15',
     'description': 'A helper package to install the latest JAX on TPUs, along with all necessary dependencies (e.g. libtpu-nightly)',
     'long_description': "# jaxtpu\n\n> A helper package to install the latest JAX on TPUs, along with all necessary dependencies (e.g. libtpu-nightly)\n\nWARNING: This repo is in development. It was automatically generated with [mkpylib](https://github.com/shawwn/scrap/blob/master/mkpylib). If you're reading this message, it means that I use this repo for my own purposes right now. It might not do anything at all; the default functionality is `print('TODO')`.\n\nIf you really want to try it out, feel free. I recommend reading through the code and commit history to see if it does what you need, or [ask me](#contact) for status updates.\n\nStay tuned!\n\n## Install\n\n```\npython3 -m pip install -U jaxtpu\n```\n\n## Usage\n\n```py\nimport jaxtpu\n\nprint('TODO')\n```\n\n## License\n\nMIT\n\n## Contact\n\nA library by [Shawn Presser](https://www.shawwn.com). If you found it useful, please consider [joining my patreon](https://www.patreon.com/shawwn)!\n\nMy Twitter DMs are always open; you should [send me one](https://twitter.com/theshawwn)! It's the best way to reach me, and I'm always happy to hear from you.\n\n- Twitter: [@theshawwn](https://twitter.com/theshawwn)\n- Patreon: [https://www.patreon.com/shawwn](https://www.patreon.com/shawwn)\n- HN: [sillysaurusx](https://news.ycombinator.com/threads?id=sillysaurusx)\n- Website: [shawwn.com](https://www.shawwn.com)\n\n",
     'long_description_content_type': 'text/markdown',
